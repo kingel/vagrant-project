f007ff2952119f4200143832af3d49f01141c618
bram vogelaar <bram@attachmentgenie.com>
2012-09-28 17:07:15 +0200

    initial import for puppet projects

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+Vagrant::Config.run do |config|
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.ssh.max_tries = 50
+  config.ssh.timeout   = 300
+  config.vm.define :puppet do |puppet_config|
+    puppet_config.vm.provision :puppet do |puppet|
+        puppet.manifests_path = "manifests"
+        puppet.module_path = "modules"
+        puppet.manifest_file = "virtualbox.pp"
+    end
+  end
+end
---
+++ manifests/virtualbox.pp
@@ -0,0 +1,27 @@
+  class { 'apt':
+    always_apt_update => true,
+  }
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com',
+    repos      => 'main',
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
+
+  case $::operatingsystem {
+    default: { $default_packages = ['subversion','git','tree','zip','unzip', 'ruby','rubygems','curl','wget'] }
+  }
+
+  package { $default_packages:
+    ensure  => latest,
+    require => Exec['apt_update'],
+  }
+
+  case $::operatingsystem {
+    default: { $gem_packages = ['librarian-puppet'] }
+  }
+
+  package { $gem_packages:
+    ensure   => latest,
+    provider => gem,
+  }
